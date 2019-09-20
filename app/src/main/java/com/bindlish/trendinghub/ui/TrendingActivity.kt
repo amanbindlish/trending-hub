554465bd6cb8eac528f733d142698b2c13a68517
Aman Bindlish <aman.bindlish@fliplearn.com>
2019-09-20 10:23:32 +0530

    separated toolbar xml and configure in activity

--- app/src/main/java/com/bindlish/trendinghub/ui/TrendingActivity.kt
+++ app/src/main/java/com/bindlish/trendinghub/ui/TrendingActivity.kt
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bindlish.trendinghub.R
 import com.bindlish.trendinghub.ui.TrendingFragment
+import kotlinx.android.synthetic.main.toolbar.*
 
 class TrendingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        // casting custom toolbar and set it as action bar
+        setSupportActionBar(toolbar)
+        // configuring behaviour of toolbar
+        supportActionBar?.setDisplayShowCustomEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, TrendingFragment.newInstance())
