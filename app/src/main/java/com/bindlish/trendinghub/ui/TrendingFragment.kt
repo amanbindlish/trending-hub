5e28366ba6975bcdb5474f2bceadd82945c6c04f
Aman Bindlish <aman.bindlish@fliplearn.com>
2019-09-20 11:07:46 +0530

    shimmer effect added in trending fragment

--- app/src/main/java/com/bindlish/trendinghub/ui/TrendingFragment.kt
+++ app/src/main/java/com/bindlish/trendinghub/ui/TrendingFragment.kt
@@ -1,13 +1,14 @@
 package com.bindlish.trendinghub.ui
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.bindlish.trendinghub.R
 import com.bindlish.trendinghub.viewmodel.TrendingViewModel
+import kotlinx.android.synthetic.main.fragment_trending.*
 
 class TrendingFragment : Fragment() {
 
@@ -21,13 +22,25 @@ class TrendingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_trending, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(TrendingViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    // starting shimmer effect in onResume
+    override fun onResume() {
+        super.onResume()
+        shimmer_view_container.startShimmer()
+    }
+
+    // stoping shimmer effect in onPause
+    override fun onPause() {
+        shimmer_view_container.stopShimmer()
+        super.onPause()
     }
 
 }
