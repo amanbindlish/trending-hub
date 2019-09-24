bc5c734105db1f06ec9819f4b644247c9d5fea11
Aman Bindlish <aman.bindlish@fliplearn.com>
2019-09-24 15:56:10 +0530

    committing viewmodel unit testing with success and failure cases

--- app/src/test/java/com/bindlish/trendinghub/viewmodel/TrendingViewModelTest.kt
+++ app/src/test/java/com/bindlish/trendinghub/viewmodel/TrendingViewModelTest.kt
@@ -3,6 +3,7 @@ package com.bindlish.trendinghub.viewmodel
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import com.bindlish.trendinghub.MockUtils
 import com.bindlish.trendinghub.data.GitRepoData
 import com.bindlish.trendinghub.data.network.Resource
 import com.bindlish.trendinghub.data.network.Status
@@ -26,7 +27,6 @@ class TrendingViewModelTest {
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
-
     @Mock
     lateinit var dataRepository: TrendingRepository
 
@@ -40,20 +40,43 @@ class TrendingViewModelTest {
 
     @Test
     fun fetchRepositories_success() {
+        // create dummy live data object
+        val repoLiveData: MutableLiveData<Resource<List<GitRepoData>>> = MutableLiveData()
+        val successResource = Resource(Status.SUCCESS, MockUtils.mockRepositories())
+        repoLiveData.postValue(successResource)
         // mock response
         Mockito.`when`(dataRepository.getRepositories()).thenAnswer {
-            return@thenAnswer MutableLiveData<Resource<List<GitRepoData>>>()
+            return@thenAnswer repoLiveData
         }
         // Attach fake observer
         val observer = Mockito.mock(Observer::class.java) as Observer<Resource<List<GitRepoData>?>>
         viewModel.getRepositoryLiveData().observeForever(observer)
-
+        // invoke data from viewModel
         viewModel.fetchUpdatedData(ArgumentMatchers.anyBoolean())
-
         // Verify and assertion
         Assert.assertNotNull(viewModel.getRepositoryLiveData().value)
         Assert.assertEquals(Status.SUCCESS, viewModel.getRepositoryLiveData().value?.status)
+    }
 
+    @Test
+    fun fetchRepositories_failure() {
+        // create dummy live data object
+        val repoLiveData: MutableLiveData<Resource<Any>> = MutableLiveData()
+        val successResource = Resource.error<Any>("some error")
+        repoLiveData.postValue(successResource)
+        // mock response
+        Mockito.`when`(dataRepository.getRepositories()).thenAnswer {
+            return@thenAnswer repoLiveData
+        }
+        // Attach fake observer
+        val observer = Mockito.mock(Observer::class.java) as Observer<Resource<List<GitRepoData>?>>
+        viewModel.getRepositoryLiveData().observeForever(observer)
+        // invoke data from viewModel
+        viewModel.fetchUpdatedData(ArgumentMatchers.anyBoolean())
+        // Verify and assertion
+        Assert.assertNotNull(viewModel.getRepositoryLiveData().value)
+        Assert.assertEquals(Status.ERROR, viewModel.getRepositoryLiveData().value?.status)
+        Assert.assertEquals("some error", viewModel.getRepositoryLiveData().value?.errorMessage)
     }
 
 }
